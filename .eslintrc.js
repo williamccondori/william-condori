b260383ea1becc56a4d95380873cc145bbd21e79
William Condori Quispe <williamccondori@outlook.com>
2021-04-14 20:52:13 -0500

    set up linter

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
